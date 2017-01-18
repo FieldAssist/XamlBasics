ba04aaf4fb9f62bfe291b4b1bfad26bdde3340c2
Ritesh Kumar <ritesh@flick2know.com>
2017-01-18 12:28:17 +0530

    [Grouping List] Added List with Grouping feature in the Project After learning about concept of ObservableCollection

--- XamlBasicsV3/XamlBasicsV3/MainPage.xaml.cs
+++ XamlBasicsV3/XamlBasicsV3/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +12,20 @@ namespace XamlBasicsV3
 {
     public partial class MainPage : ContentPage
     {
-        public List<Entity> TheList { get; set; }
+        public ObservableCollection<Grouping<string, Entity>> TheList { get; set; }
         public MainPage()
         {
             Init();
             InitializeComponent();
-            BindingContext = this;
+            BindingContext = TheList;
         }
 
-        private async Task Init()
+        private void Init()
         {
-            TheList = await DataGenerator.getGeneratedEntities();
-
+            var listOfEntities = DataGenerator.getGeneratedEntities();
+            var sorted = listOfEntities.OrderBy(x => x.StringValue).GroupBy(c => c.StringValue[0]).
+                Select(thegroup => new Grouping<string, Entity>(thegroup.Key.ToString(), thegroup));
+            TheList = new ObservableCollection<Grouping<string, Entity>>(sorted);
         }
 
         public void OnItemTapped(object o, ItemTappedEventArgs e)
---
+++ XamlBasicsV3/XamlBasicsV3/Models/Grouping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlBasicsV3.Models
+{
+    public class Grouping<K, T> : ObservableCollection<T>
+    {
+        public K Key { get; private set; }
+        public Grouping(K Key, IEnumerable<T> items)
+        {
+            this.Key = Key;
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
